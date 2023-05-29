5927309e90ae0dc4ef63808db08cb89c0f57b90b
Taris Henrique <taris.tj@hotmail.com>
2023-05-29 11:20:02 -0300

    Atualizacao de Medicos funcionando

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosAtualizarMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -14,6 +15,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +43,13 @@ public class MedicoController {
     @GetMapping("/medicos")
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
     	return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+    
+    @PutMapping("/medicos")
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizarMedico dados) {
+    	var medico = repository.getReferenceById(dados.id()) ;
+    	medico.atualizarInformacoes(dados);
     }
 
 
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -35,6 +36,23 @@ public class Medico {
 		this.crm = dados.crm();
 		this.especialidade = dados.especialidade();
 		this.endereço = new Endereco(dados.endereco());
+		
+	}
+
+	public void atualizarInformacoes(@Valid DadosAtualizarMedico dados) {
+		
+		if (dados.nome() != null) {
+		this.nome = dados.nome();
+		}
+		
+		if(dados.telefone() != null) {
+			this.telefone = dados.telefone();
+		}
+		
+		if(dados.endereco() != null) {
+			this.endereço.atualizarInformacoes(dados.endereco());
+		}
+		
 		
 	}
 	
